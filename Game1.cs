26e594eebbfad08f766f5db4e094e8c9e3a2ad88
Miss Darling <TheCanterlonian@users.noreply.github.com>
2025-01-11 01:58:12 -0800

    Update Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -68,8 +68,8 @@ public class Game1 : Game
 		spriteBatch.Begin();//print spritesheet for debug
 		spriteBatch.Draw(spritesheet, pos, Colour.White);
 		spriteBatch.End();
-  		int[,] origin = {[0], [0]}
-    		int[,] size = {[10], [10]}
+  		int[] origin = {0, 0}
+    		int[] size = {10, 10}
 		Box.BoxMaker(origin, size, graphics)
 		
 		
